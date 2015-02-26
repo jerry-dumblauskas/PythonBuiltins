36041ad1dcd63850444e76e591ebd84b3bbe1166
jerry dumblauskas <jdumblauskas@gmail.com>
2015-02-26 07:21:46 -0600

    testing relative imports -- this is odd -- and Pycharm seems to have some issues (or it is me)

---
+++ rel_imp_work/mod_that_does_the_rel_import.py
@@ -0,0 +1,12 @@
+__author__ = 'jerrydumblauskas'
+
+from mod_to_be_relative_imported import Worker
+
+'''
+why doesn't this work?
+'''
+
+print Worker().caller()
+
+if __name__ == "__main__":
+    print "boom"
---
+++ rel_imp_work/mod_to_be_relative_imported.py
@@ -0,0 +1,12 @@
+__author__ = 'jerrydumblauskas'
+
+class Worker():
+    '''
+    doing some tests on relative importing.....ugh pycharm
+    '''
+    def __init__(self):
+        self.A = "test"
+
+    def caller(self):
+        return self.A
+
