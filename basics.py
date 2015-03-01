5a3eba78d6be205c3667098dcf6437431d3d76b3
jerry dumblauskas <jdumblauskas@gmail.com>
2015-03-01 08:03:00 -0600

    start using the term splat, and start documenting itertools

--- basics.py
+++ basics.py
@@ -5,7 +5,7 @@ p = zip(mn_list,mx_list)
 print "zip output -- a list of tuples"
 print p
 
-print "wrap syntax -- if you call a function with a *x, it is an iterable that will be uppacked into the function"
+print "wrap syntax -- if you call a function with a *x, it is an iterable that will be uppacked, into the function"
 print "if it is in the func arg list, the values will be wrapped into a tuple"
 lst = [1,2]
 
@@ -17,15 +17,15 @@ def wrap_in_a_tuple(*g):
     print type(g)
     print g
 
-def unpack_vars(t, h):
+def splat_vars(t, h):
     '''
-    fan out
+    fan out or show how we "SPLAT"
     '''
     print t
     print h
 
 # fan out
-unpack_vars(*lst)
+splat_vars(*lst)
 # fan in
 wrap_in_a_tuple(lst, 3)
 
@@ -35,7 +35,7 @@ print zip(*p)
 
 print "DECORATOR TESTS"
 
-print "OK 1========================"
+print "OK 1========================(better, as it has the var args list)"
 def p_decorate(func):
    def func_wrapper(*x):
        return "<p>{0}</p>".format(func(*x))
@@ -60,3 +60,15 @@ def get_text1(name):
    return "lorem ipsum, {0} dolor sit amet".format(name)
 
 print get_text1("John")
+
+
+print "ITERTOOLS!"
+
+from itertools import chain
+
+q=[1,2,3,4,5,6,7,8,9]
+p=[3,4,5,3,2,4,5,6,7]
+y = chain(q,p)
+
+for t in y:
+    print t
