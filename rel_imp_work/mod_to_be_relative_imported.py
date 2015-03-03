7c64e53865cd3cdc8bfe3ca939cb0ab6db17d7b3
Jerry Dumblauskas <jdumblauskas@gmail.com>
2015-03-03 16:37:14 -0600

    typo

--- rel_imp_work/mod_to_be_relative_imported.py
+++ rel_imp_work/mod_to_be_relative_imported.py
@@ -5,7 +5,7 @@ class Worker():
     doing some tests on relative importing.....
     if I call from the shell from the root of this project like so
     python -m rel_imp_work.mod_that_does_the_rel_import
-    works fine -- beause of peo 366
+    works fine -- because of peo 366
     '''
     def __init__(self):
         self.A = "test"
