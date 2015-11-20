c55b128ade522d476f52907a2883e166f685e602
jerry-dumblauskas <jdumblauskas@gmail.com>
2015-11-20 08:38:35 -0600

    more python 3 conversion

--- rel_imp_work/mod_that_does_the_rel_import.py
+++ rel_imp_work/mod_that_does_the_rel_import.py
@@ -6,11 +6,11 @@ from .mod_to_be_relative_imported import Worker
 why doesn't this work? -- cause -- you can't call in the same directory see pep 366
 '''
 
-print Worker().caller()
+print (Worker().caller())
 
 def junk():
-    print "done"
+    print ("done")
 
 
 if __name__ == "__main__":
-    print "boom"
+    print ("boom")
