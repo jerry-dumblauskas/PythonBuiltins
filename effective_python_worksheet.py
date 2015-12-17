f1dc3f02985ae7fb6ef909837502089c6bdcac57
jerry-dumblauskas <jdumblauskas@gmail.com>
2015-12-17 07:08:58 -0600

    #38

--- effective_python_worksheet.py
+++ effective_python_worksheet.py
@@ -828,7 +828,7 @@ print('Finished in %.3f seconds' % (end - start))
 print("====ITEM 37: Use Threads for Blocking I/O, Avoid for Parallelism ====")
 print("thi gil!!  But if you have system io, try threads")
 import select
-from threading import Thread
+from threading import Thread, Lock
 
 
 def slow_select():
@@ -851,9 +851,52 @@ for thread in threads:
 end = time()
 print('using IO Took %.3f seconds' % (end - start))
 
-# Item __ ...
-print("====ITEM  ====")
-print("....")
+# Item 38 ...
+print("====ITEM 38: Use Lock to Prevent Data Races in Threads ====")
+print("even with the gil -- need to lock critical sections")
+
+print("bad way")
+class Counter(object):
+    def __init__(self):
+        self.count = 0
+
+    def increment(self, offset):
+        self.count += offset
+
+def worker(sensor_index, how_many, counter):
+    for _ in range(how_many):
+        counter.increment(1)
+
+def run_threads(func, how_many, counter):
+    threads = []
+    for i in range(5):
+        args = (i, how_many, counter)
+        thread = Thread(target=func, args=args)
+        threads.append(thread)
+        thread.start()
+    for thread in threads:
+        thread.join()
+
+how_many = 10**5
+counter=Counter()
+run_threads(worker, how_many, counter)
+print ("Counter should be %d, found %d" % (5 * how_many, counter.count))
+
+print("works")
+
+class LockingCounter(object):
+    def __init__(self):
+        self.lock = Lock()
+        self.count = 0
+
+    def increment(self, offset):
+        with self.lock:
+            self.count += offset
+
+counter = LockingCounter()
+run_threads(worker, how_many, counter)
+print('Counter should be %d, found %d' %
+      (5 * how_many, counter.count))
 
 # Item __ ...
 print("====ITEM  ====")
