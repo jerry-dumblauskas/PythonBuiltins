82c4d06fdbcdc0e0a7a21ab37e1fd85d972d1141
jerry-dumblauskas <jdumblauskas@gmail.com>
2015-12-28 07:21:11 -0600

    #58

--- effective_python_worksheet.py
+++ effective_python_worksheet.py
@@ -1291,8 +1291,54 @@ print("use your ide for the most part :)")
 
 # Item 58 ...
 print("====ITEM 58: Profile Before Optimizing ====")
-print("....")
+print("use CProfile -- faster than the pure python version")
+def insertion_sort(data):
+    result = []
+    for value in data:
+        insert_value(result, value)
+    return result
 
-# Item __ ...
-print("====ITEM  ====")
-print("....")
+def insert_value(array, value):
+    for i, existing in enumerate(array):
+        if existing > value:
+            array.insert(i, value)
+            return
+    array.append(value)
+
+from random import randint
+
+max_size = 10**4
+data = [randint(0, max_size) for _ in range(max_size)]
+test = lambda: insertion_sort(data)
+
+from cProfile import  Profile
+
+pp = Profile()
+pp.runcall(test)
+
+from pstats import Stats
+
+stats = Stats(pp)
+stats.strip_dirs()
+stats.sort_stats('cumulative')
+print("slow======")
+stats.print_stats()
+
+from bisect import bisect_left
+
+def insert_value(array, value):
+    i = bisect_left(array, value)
+    array.insert(i, value)
+
+test = lambda: insertion_sort(data)
+pp = Profile()
+pp.runcall(test)
+stats = Stats(pp)
+stats.strip_dirs()
+stats.sort_stats('cumulative')
+print("fast==== note the total time compared to the last run")
+stats.print_stats()
+
+# Item 59 ...
+print("====ITEM 59: Use tracemalloc to Understand Memory Usage and Leaks ====")
+print("only in 3.4 and above")
