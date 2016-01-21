cbf9e966b2097d9f303ff9aec1577ad21db1cae3
jerry-dumblauskas <jdumblauskas@gmail.com>
2016-01-21 07:35:28 -0600

    start of mocking tutorial

---
+++ my_mocks/mock_worksheet.py
@@ -0,0 +1,7 @@
+"""
+This file will hold both classes and methods used to demonstrate mock
+usage
+"""
+
+def first_method():
+    return 0
---
+++ my_mocks/test_mock_worksheet.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+"""
+unit test module to demonstrate mocking
+this will call items from mock_worksheet
+"""
+class TestFirst_method(TestCase):
+    def test_first_method(self):
+        self.assertTrue(1)
