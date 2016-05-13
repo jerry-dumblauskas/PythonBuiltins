d3ad895f445a9c83417eb76f1a252db6a9458532
jerry-dumblauskas <jdumblauskas@gmail.com>
2016-05-13 16:10:57 -0500

    wow -- that cut a lot of lines....

--- puzzlers/anagrams.py
+++ puzzlers/anagrams.py
@@ -8,12 +8,4 @@ def check_them(tst1, tst2):
     :param tst2:
     :return: True if words are an anagram, False if not
     """
-    if len(tst1) != len(tst2):
-        return False
-
-    tst2_sorted = sorted(tst2)
-
-    for pos, item in enumerate(sorted(tst1)):
-        if item != tst2_sorted[pos]:
-            return False
-    return True
+    return sorted(tst1) == sorted(tst2)
