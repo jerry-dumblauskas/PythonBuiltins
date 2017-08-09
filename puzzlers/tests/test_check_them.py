ea89c17ca168929a9919469707cc2eb1f98e884c
jerry-dumblauskas <jdume@allstate.com>
2017-08-09 09:29:14 -0500

    add test case

--- puzzlers/tests/test_check_them.py
+++ puzzlers/tests/test_check_them.py
@@ -13,3 +13,6 @@ class TestCheckThem(TestCase):
 
     def test_check_them_not_an_anagram_same_length(self):
         self.assertEqual(False, check_them('pep', 'opp'))
+
+    def test_check_them_case(self):
+        self.assertEqual(False, check_them('Pep', 'pep'))
