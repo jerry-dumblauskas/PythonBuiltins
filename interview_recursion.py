7b1023d6d4d683c1f120821d77d0f95c52e25f4e
jerry-dumblauskas <jdumblauskas@gmail.com>
2017-12-08 16:04:48 -0600

    fix answer for dean question

--- interview_recursion.py
+++ interview_recursion.py
@@ -1,35 +1,13 @@
-def check_it(in_lst):
-    """
-    this function accepts a sorted list, then looks for the ONE missing number in the list
-    the missing number can't be at the start or the end of the list
-    :param in_lst:
-    :return: the missing number
-    """
+def check_it_easy(in_x):
+    return sum(range(in_x[-1] + 1)) - sum(in_x)
 
-    st_val = in_lst[0]
-    half_pt = len(in_lst) / 2
-    half_val = in_lst[int(half_pt)]
-
-    # final exit point -- when 2 items are left in the list
-    if len(in_lst) <= 2:
-        return in_lst[0] + 1
-
-    # Slicing logic, keep cutting down till 2 points are left
-    if half_val == half_pt + st_val:
-        rtn = check_it(in_lst[int(half_pt):])
-    else:
-        rtn = check_it(in_lst[:int(half_pt) + 1])
-    return rtn
-
-def check_it_easy(x):
-    return sum(range(x[-1] + 1)) - sum(x)
 
 if __name__ == "__main__":
     x = [0, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    assert check_it(x) == 1
-    print(check_it(x))
-    print (check_it_easy(x))
+    print(check_it_easy(x))
     x = list(range(100))
-    x.remove(0)
     print(x)
-    print(check_it(x))
+    x.remove(0)
+    x.remove(55)
+    print(x)
+    print(check_it_easy(x))
