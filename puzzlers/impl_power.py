65abf3447f20835d36d3264a3e3ccde9ea4d87f9
jerry-dumblauskas <jdumblauskas@gmail.com>
2017-12-08 15:56:39 -0600

    pep 8 on power script

--- puzzlers/impl_power.py
+++ puzzlers/impl_power.py
@@ -4,13 +4,16 @@ interview question -- implement power, without a for loop, and without using pow
 from functools import reduce
 from operator import mul
 
+
 def answer1(bse, pwr):
     if pwr == 0:
         return 1
-    return answer1(bse, pwr-1) * bse
+    return answer1(bse, pwr - 1) * bse
+
 
 def answer2(bse, pwr):
     return reduce(mul, [bse] * pwr)
+
 
 if __name__ == "__main__":
     tst_answer = answer1(2, 5)
