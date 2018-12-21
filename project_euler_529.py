63b893344361bdaf8c148703f21d09f4907e5891
Jerry <jdumblauskas@gmail.com>
2018-12-20 19:19:30 -0600

    simpler -- but still the runtime is too long

--- project_euler_529.py
+++ project_euler_529.py
@@ -5,22 +5,26 @@ def do_it(in_num):
     for x in range(pow(10, in_num) + 1):
         # if x != 3523014:
         #     continue
-        # if x != 5:
-        #     continue
+        # if x == 190:
+        #     print("break")
         lst = []
+        sub_x = x
         if x > 10:
             short_circuit = hot_items.get(int(str(x)[:-1]), 0)
             if short_circuit != 0:
                 last = short_circuit
                 real_last = int(str(x)[-1])
-                if last + real_last == 10:
+                if (last + real_last == 10) or real_last == 0:
                     acc += 1
+                    print(x)
                     hot_items[x] = int(str(x)[-1])
                     continue
-        for y in str(x):
+
+        for y in str(sub_x):
             lst.append(int(y))
         if x % 1000000 == 0:
-            print("len is", x)
+            import datetime
+            print("len is", x, datetime.datetime.now())
 
         pos_lst = [0 for _x in range(len(lst))]
 
@@ -30,6 +34,7 @@ def do_it(in_num):
                 pos_lst[p] = 1
         if all(pos_lst):
             acc += 1
+            print(x)
             hot_items[x] = int(str(x)[-1])
     return acc
 
@@ -60,5 +65,55 @@ def do_algo(x, in_lst):
     return []
 
 
+def do_it2(int_in):
+    work_unit = str(int_in)
+    st = 0
+    end = len(work_unit)
+    truth_list = [0 for _x in range(end)]
+    tmp = 0
+    while st < end:
+        sub_while = st
+        while tmp < 10:
+            variable_under_the_scope = int(work_unit[sub_while])
+            tmp = tmp + variable_under_the_scope
+            sub_while += 1
+            if sub_while >= end:
+                break
+
+        if tmp == 10:
+            for p in range(st, sub_while):
+                truth_list[p] = 1
+        tmp = 0
+
+        st += 1
+
+    # trailing zero hack
+    for x, val in enumerate(work_unit):
+        if int(val) == 0:
+            if truth_list[int(x) - 1] == 1:
+                truth_list[int(x)] = 1
+    return truth_list
+
+
 if __name__ == '__main__':
-    print(do_it(18))
+    # print(do_it2(3523014))
+    # print(do_it2(28546))
+    import datetime
+    start_time = datetime.datetime.now()
+    print(start_time)
+    cnt = 0
+    for x1 in range(pow(10, 7) + 1):
+        tst = do_it2(x1)
+        if x1 % 1000000 == 0:
+
+            print("len is", x1, )
+        if all(tst):
+            cnt += 1
+            # print(x)
+    print(cnt)
+    end_time = datetime.datetime.now()
+    print(end_time)
+    print(end_time - start_time)
+
+    # print(do_it2(190))
+    # assert 3492 == do_it(5)
