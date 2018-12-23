d605333126e8a89ad58dfe088caeecb028223d84
Jerry <jdumblauskas@gmail.com>
2018-12-23 12:15:24 -0600

    add in cache

--- project_euler_529.py
+++ project_euler_529.py
@@ -1,18 +1,30 @@
 import re
 
-matcher = re.compile("([2-9]99[2-9])|([3-9]88[3-9])")
+cache = {}
+matcher = re.compile("([2-9]99[2-9])|([3-9]88[3-9])|([4-9]77[4-9])")
+ending_zeros = re.compile("(.)(0+$)")
 
 
 def do_it(int_in):
+    # if int_in == 9000:
+    #     print("bummer")
     work_unit = str(int_in)
     st = 0
     end = len(work_unit)
     truth_list = [0 for _x in range(end)]
     tmp = 0
 
+    # Filter on bad patterns
     xx = matcher.search(work_unit)
     if xx:
         return truth_list
+
+    # Try and use a cache and regular expressions
+    pp = ending_zeros.search(work_unit)
+    if pp:
+        if cache.get(pp.group(1)) == 1:
+            return [1 for _x in range(end)]
+
     # end filter
     while st < end:
         sub_while = st
@@ -41,21 +53,19 @@ def do_it(int_in):
 if __name__ == '__main__':
 
     import datetime
+
     start_time = datetime.datetime.now()
     print(start_time)
     cnt = 0
-    for x1 in range(pow(10, 6) + 1):
+    power_of_ten = 7
+    for x1 in range(pow(10, power_of_ten) + 1):
         tst = do_it(x1)
         if x1 % 1000000 == 0:
-
-            print("len is", x1, )
+            print("len is", x1, datetime.datetime.now())
         if all(tst):
             cnt += 1
+            cache[str(x1)] = 1
             # print(x1)
     print(cnt)
     end_time = datetime.datetime.now()
-    print(end_time)
     print(end_time - start_time)
-
-    # print(do_it(190))
-    # assert 3492 == do_it(5)
