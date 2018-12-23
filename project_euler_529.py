8b4e46b8d4e53aae0250c64d6900529f2f4069be
jerry-dumblauskas <jdumblauskas@gmail.com>
2018-12-23 17:01:00 -0600

    strip 0's

--- project_euler_529.py
+++ project_euler_529.py
@@ -2,13 +2,12 @@ import re
 
 cache = {}
 matcher = re.compile("([2-9]99[2-9])|([3-9]88[3-9])|([4-9]77[4-9])")
-ending_zeros = re.compile("(.+)(0+$)")
 
 
 def do_it(int_in):
-    # if int_in == 9000:
+    # if int_in == 0:
     #     print("bummer")
-    work_unit = str(int_in)
+    work_unit = str(int_in).replace("0", "")
     st = 0
     end = len(work_unit)
     truth_list = [0 for _x in range(end)]
@@ -19,11 +18,9 @@ def do_it(int_in):
     if xx:
         return truth_list
 
-    # Try and use a cache and regular expressions
-    pp = ending_zeros.search(work_unit)
-    if pp:
-        if cache.get(pp.group(1)) == 1:
-            return [1 for _x in range(end)]
+    # Try and use a cache (since we are stripping strings)
+    if cache.get(work_unit) == 1:
+        return [1 for _x in range(end)]
 
     # end filter
     while st < end:
@@ -39,14 +36,8 @@ def do_it(int_in):
             for p in range(st, sub_while):
                 truth_list[p] = 1
         tmp = 0
-
         st += 1
 
-    # trailing zero hack
-    for x, val in enumerate(work_unit):
-        if int(val) == 0:
-            if truth_list[int(x) - 1] == 1:
-                truth_list[int(x)] = 1
     return truth_list
 
 
@@ -62,7 +53,7 @@ if __name__ == '__main__':
         tst = do_it(x1)
         if x1 % 1000000 == 0:
             print("len is", x1, datetime.datetime.now())
-        if all(tst):
+        if tst and all(tst):
             cnt += 1
             cache[str(x1)] = 1
             # print(x1)
