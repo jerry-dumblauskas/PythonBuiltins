9369753ee0698b71e370a628cb4590f31be611c2
jerry dumblauskas <jdumblauskas@gmai.com>
2023-02-26 13:35:35 -0600

    fancy sieve algo -- runs fastgit add .

--- project_euler/project_euler_10.py
+++ project_euler/project_euler_10.py
@@ -1,35 +1,37 @@
+import math
+
 """
 The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 Find the sum of all the primes below two million.
 """
 
-"""
-this runs to0 long.  need to get it in under a minute
-have to get an algorithm to not check every number
-142913828922
-"""
 
-def is_prime(in_num):
-    if in_num <= 0:
-        raise Exception(" invalid number, make it > 0")
-    if in_num < 3:
-        return True
-    if in_num % 2 == 0:
-        return False
+def find_primes(in_max):
+    num_sqrt = int(math.sqrt(in_max))
+    raw_primes_lst = list(range(in_max + 1))
+    iterating_lst = list(range(in_max + 1))
+    for i in range(len(iterating_lst)):
+        if i in (0, 1):
+            raw_primes_lst[i] = 0
+            continue
+        run_var = int(math.pow(i, 2))
+        while True:
+            if run_var > in_max:
+                break
+            raw_primes_lst[run_var] = 0
+            run_var += i
 
-    for chk in range(3, int(in_num / 2), 2):
-        if in_num % chk == 0:
-            return False
-    return True
+        if iterating_lst[i] == num_sqrt:
+            break
+
+    final_primes_lst = []
+    for y in raw_primes_lst:
+        if y > 0:
+            final_primes_lst.append(y)
+    return final_primes_lst
 
 
-the_sum = 0
-potential_prime = 2000000
-while potential_prime > 1:
-    if is_prime(potential_prime):
-        the_sum += potential_prime
-        print(potential_prime)
-    potential_prime -= 1
-
-print(the_sum)
+if __name__ == '__main__':
+    x = find_primes(2000000)
+    print(sum(x))
